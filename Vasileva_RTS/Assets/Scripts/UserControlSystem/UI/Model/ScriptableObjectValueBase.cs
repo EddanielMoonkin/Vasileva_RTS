cd71a5cdce6934b97e0712afc95b5ad38fcf6d31
EddanielMoonkin <eddanielmoonkin@gmail.com>
2023-06-11 18:27:58 +0300

    lesson4 hometask

--- Vasileva_RTS/Assets/Scripts/UserControlSystem/UI/Model/ScriptableObjectValueBase.cs
+++ Vasileva_RTS/Assets/Scripts/UserControlSystem/UI/Model/ScriptableObjectValueBase.cs
@@ -3,12 +3,9 @@ using UnityEngine;
 
 public abstract class ScriptableObjectValueBase<T> : ScriptableObject, IAwaitable<T>
 {
-    public class NewValueNotifier<TAwaited> : IAwaiter<TAwaited>
+    public class NewValueNotifier<TAwaited> : AwaiterBase<TAwaited>
     {
-        private readonly ScriptableObjectValueBase<TAwaited> _scriptableObjectValueBase;
-        private TAwaited _result;
-        private Action _continuation;
-        private bool _isCompleted;
+        private readonly ScriptableObjectValueBase<TAwaited> _scriptableObjectValueBase;       
 
         public NewValueNotifier(ScriptableObjectValueBase<TAwaited> scriptableObjectValueBase)
         {
@@ -19,21 +16,8 @@ public abstract class ScriptableObjectValueBase<T> : ScriptableObject, IAwaitabl
         private void onNewValue(TAwaited obj)
         {
             _scriptableObjectValueBase.OnNewValue -= onNewValue;
-            _result = obj;
-            _isCompleted = true;
-            _continuation?.Invoke();     
-        }
-
-        public void OnCompleted(Action continuation)
-        {
-            if(_isCompleted)
-                continuation?.Invoke();
-            else
-                _continuation = continuation;
-        }
-
-        public bool IsCompleted => _isCompleted;
-        public TAwaited GetResult() => _result;
+            onWaitFinish(obj);
+        }      
         
     }
 
